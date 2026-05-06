d1db025e9d1b89ee814cd03186b530e689bfe385
armrest99 <amrithkrishnakumar99@gmail.com>
2026-05-06 23:04:09 +0000

    Break things for testing lint check

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os
+import sys
+import numpy as np
+
 def main():
+    x=1+2
+    unused_var = "hello"
     print("Hello from dsc190-assignment05!")
+    y = x + 1
+    return y
+
 
 
 if __name__ == "__main__":
